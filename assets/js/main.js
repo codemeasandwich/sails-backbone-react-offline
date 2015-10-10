66c663836a19a8d2d792830bc925749eddc246f7
Brian Shannon <brian.shann@gmail.com>
2015-10-10 15:08:32 +0100

    backbone loading with requireJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
+"use strict"
+ 
+require.config({
+  baseUrl: '../',
+  paths: {
+    jquery:["vendor/jquery/dist/jquery"],
+    underscore:["vendor/lodash-amd/modern/main"],
+    backbone:["vendor/backbone/backbone"]
+  },
+ shim: {
+		underscore: {
+			exports: '_'
+		},
+		backbone: {
+			deps: [
+				'underscore',
+				'jquery'
+			],
+			exports: 'Backbone'
+		}
+	}
+});
 
+console.log("require main");
+
+require(["backbone"], (Backbone)=>{
+      console.log("arguments",arguments);
+      console.log("Backbone",Backbone);
+});
